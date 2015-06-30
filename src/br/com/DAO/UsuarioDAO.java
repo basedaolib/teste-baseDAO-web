36d2d5bcfd3c7e4b03b75ccecc5406e706ec1bf2
henrique.luiz <henrique.luiz@Henrique-desenvolvimento.datarey.info>
2015-06-30 17:23:11 -0300

    inicio

---
+++ src/br/com/DAO/UsuarioDAO.java
@@ -0,0 +1,8 @@
+package br.com.DAO;
+
+import br.com.baseDAOLib.DAO.BaseDAO;
+import br.com.entity.Usuario;
+
+public interface UsuarioDAO extends BaseDAO<Usuario> {
+
+}
---
+++ src/br/com/DAO/UsuarioDAOImpl.java
@@ -0,0 +1,7 @@
+package br.com.DAO;
+
+import br.com.baseDAOLib.DAO.BaseDAOImpl;
+import br.com.entity.Usuario;
+
+public class UsuarioDAOImpl extends BaseDAOImpl<Usuario> implements UsuarioDAO{
+}
