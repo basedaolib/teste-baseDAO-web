282feb6d061d15cbd2d3f99e5976d7f1154f80d9
henrique.luiz <henrique.luiz@Henrique-desenvolvimento.datarey.info>
2015-07-02 17:44:42 -0300

    adição

---
+++ src/view/OpenSession.java
@@ -0,0 +1,58 @@
+package view;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class OpenSession
+ */
+@WebFilter("/OpenSession")
+public class OpenSession implements Filter {
+
+	@Inject
+	private EntityManager manager;
+	
+    /**
+     * Default constructor. 
+     */
+    public OpenSession() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+
+		// pass the request along the filter chain
+		manager.getTransaction().begin();
+		chain.doFilter(request, response);
+		manager.getTransaction().commit();
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
